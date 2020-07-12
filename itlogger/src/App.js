41aa13666086ccea88a8bad250de0383048d876b
Gbenga <gbengulo005@gmail.com>
2020-07-12 20:50:57 +0100

    navbar component

--- itlogger/src/App.js
+++ itlogger/src/App.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect, Fragment } from "react";
+import SearchBar from "./components/layout/SearchBar";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
 
 const App = () => {
-  return <div className="App">My App</div>;
+  useEffect(() => {
+    //Init Materialize Js
+    M.AutoInit();
+  });
+  return (
+    <Fragment>
+      <SearchBar />
+    </Fragment>
+  );
 };
 
 export default App;
