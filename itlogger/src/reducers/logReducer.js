0a95cff1b617fb019550c784d5fa745acc241169
Gbenga <gbengulo005@gmail.com>
2020-07-30 14:49:31 +0100

    log actions implementation

--- itlogger/src/reducers/logReducer.js
+++ itlogger/src/reducers/logReducer.js
@@ -1,3 +1,10 @@
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGs_ERROR,
+  LOGS_ERROR,
+} from "../actions/types";
+
 const initialState = {
   logs: null,
   current: null,
@@ -7,6 +14,23 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case GET_LOGS:
+      return {
+        ...state,
+        logs: action.payload,
+        loading: false,
+      };
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    case LOGS_ERROR:
+      console.error(action.payload);
+      return {
+        ...state,
+        error: action.payload,
+      };
     default:
       return state;
   }
